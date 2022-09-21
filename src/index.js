3139494d3f6defade4bad0188f3a470f5751c6fc
andreCaetano225 <you@example.com>
2022-09-20 22:25:38 -0300

    20-09-2022 Realizando requisao com API do analitico

--- src/index.js
+++ src/index.js
@@ -1,28 +1,53 @@
 const express = require('express');
+const {uuid} = require('uuidv4')
 
 const app = express();
 
+app.use(express.json());
+
+const projects = []
+
+
 app.get('/projects', (request,response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2' 
-    ])
+    // const {title, owner } = request.query
+
+    // console.log(title)
+    // console.log(owner)
+
+
+    return response.json(projects)
 })
 
 app.post('/projects', (request,response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ])
+    const {title, owner} = request.body
+
+    const project = {id: uuid(), title, owner}
+
+    projects.push(project)
+
+    return response.json(project)
 })
 
 app.put('/projects/:id', (request,response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ])
+    const {id} = request.params
+    const {title, owner} = request.body
+
+    const projetcIndex = projects.findIndex(project => project.id === id);
+
+    if(projetcIndex < 0){
+        return response.status(400).json({ erro: 'Projet not found'})
+    }
+
+    const projetc = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projetcIndex] = projects;
+
+    console.log(params)
+    return response.json(projetc)
 })
 
 app.delete('/projects/:id', (request,response) => {
